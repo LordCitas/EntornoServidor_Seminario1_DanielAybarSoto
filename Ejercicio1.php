f3971c6dd2c5b3d34dc64ed4ee93080c8cb8865d
LordCitas <dantre340@gmail.com>
2025-10-15 11:40:32 +0200

    uy 2.0

--- Ejercicio1.php
+++ Ejercicio1.php
@@ -1,2 +1,15 @@
 <?php
-function arrayMax
+
+function arrayMax($array){
+    $max = $array[0];
+    for($i = 1; $i < count($array); $i++){
+        if ($array[$i] > $max){
+            $max = $array[$i];
+        }
+    }
+    return $max;
+}
+
+$array = [1,2,3,4,5,6,7,8,5,9];
+echo arrayMax($array);
+
