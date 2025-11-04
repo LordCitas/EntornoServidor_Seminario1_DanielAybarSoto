d6569504976db3b457312f40ac0660d235e0fd89
LordCitas <dantre340@gmail.com>
2025-11-04 14:25:37 +0100

    ejercicio13 sin terminar

--- Ejercicio13.php
+++ Ejercicio13.php
@@ -3,15 +3,28 @@ function etiqueta ($linea): string{
     $resultado = "<";
 }
 
+declare(strict_types=1);
+
 try{
     //El número que vamos a usar para probar la función
-    $linea = "a";
+    $cadena = "a";
 
     /*
     //Un valor erróneo
     $numero = "hola";
     */
 
+    if(count(preg_split('/[#.]/', $cadena)) == )
+
+    $separacion = preg_split('/[#.]/', $cadena);
+
+    $resultado = $separacion[0];
+
+    foreach ($separacion as $cadena) {
+
+    }
+
+.a#c.v
     //Si el dato no es válido, lanzamos la excepción
     if(is_numeric($numero)){
         echo $numero . ((esCapicua($numero))?"": " NO") . " es capicúa";
