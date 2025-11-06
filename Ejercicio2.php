627315bc1dafe2f681132cda0fdb74aa2016a408
LordCitas <dantre340@gmail.com>
2025-11-06 23:32:44 +0100

    Comentarios

--- Ejercicio2.php
+++ Ejercicio2.php
@@ -1,12 +1,18 @@
 <?php
-
+//Una función que calcula el sumatorio de los elementos de un array
 function sumatorioArray($array){
+    //Inicializamos la variable sumatorio
     $sumatorio = 0;
+
+    //Recorremos el array y sumamos sus elementos
     foreach($array as $elemento){
         $sumatorio += $elemento;
     }
+
+    //Devolvemos el sumatorio
     return $sumatorio;
 }
 
+//Definimos el array y mostramos el resultado
 $lista = [1,2,3,4,5,6,7,8,9,34,53,24,21,23,21,1,3];
 echo sumatorioArray($lista);
--- Ejercicio3.php
+++ Ejercicio3.php
@@ -1,8 +1,21 @@
 <?php
-
-function pasarMillasAKilometros($millas){
+//Una función que convierte millas a kilómetros
+function pasarMillasAKilometros(float $millas): float{
+    //Devolvemos el valor convertido
     return $millas*1.60934;
 }
 
+//Definimos las millas a convertir
 $millas = 100;
-echo $millas . " millas equivalen a " . pasarMillasAKilometros(100) . " kilómetros.";
+
+//Llamamos a la función dentro de un bloque try-catch
+try{
+    //Validamos que el valor es numérico y positivo
+    if (!is_numeric($millas) || $millas < 0){
+        throw new UnexpectedValueException;
+    }
+    //Mostramos el resultado
+    echo $millas . " millas equivalen a " . pasarMillasAKilometros(100) . " kilómetros.";
+} catch (UnexpectedValueException $e){
+    echo "El valor de millas debe ser un número positivo.";
+}
